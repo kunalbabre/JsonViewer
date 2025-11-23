9f69c3f5e781f09e2f12899632f05bea41914cda
Kunal Babre <kunalbabre@gmail.com>
2025-11-23 16:36:30 +0000

    Add error handling for message sending in background script to ensure stability

--- src/background.js
+++ src/background.js
@@ -1,5 +1,7 @@
 chrome.action.onClicked.addListener((tab) => {
   if (tab.id) {
-    chrome.tabs.sendMessage(tab.id, { action: 'toggle' });
+    chrome.tabs.sendMessage(tab.id, { action: 'toggle' }).catch(() => {
+      // Ignore error if content script is not ready or page is not supported
+    });
   }
 });
